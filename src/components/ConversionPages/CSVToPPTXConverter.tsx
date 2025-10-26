4679ec18fcdfa8aa2a478111b622feef6f554fb4
goreckis6 <116648161+goreckis6@users.noreply.github.com>
2025-10-26 10:52:54 +0100

    fdf

--- src/components/ConversionPages/CSVToPPTXConverter.tsx
+++ src/components/ConversionPages/CSVToPPTXConverter.tsx
@@ -225,15 +225,6 @@ export const CSVToPPTXConverter: React.FC = () => {
                 </div>
               )}
 
-              {/* Conversion Time Info */}
-              <div className="mt-6 p-4 bg-blue-50 border border-blue-200 rounded-lg">
-                <div className="flex items-center">
-                  <Clock className="w-5 h-5 text-blue-500 mr-2" />
-                  <span className="text-sm text-blue-700 font-medium">
-                    {t('csv_to_pptx.conversion_time_info')}
-                  </span>
-                </div>
-              </div>
 
               <div className="mt-8">
                 <button
