b0f110a05b9288f11924629824d17e2160c7b30e
goreckis6 <116648161+goreckis6@users.noreply.github.com>
2026-01-11 23:17:07 +0100

    Fix redirects to use RequestEvent.redirect for Qwik City

--- src/routes/[lang]/converter/index.tsx
+++ src/routes/[lang]/converter/index.tsx
@@ -1,8 +1,7 @@
 import type { RequestHandler } from '@builder.io/qwik-city';
-import { redirect } from '@builder.io/qwik-city';
 
-export const onGet: RequestHandler = ({ url }) => {
-  const segs = url.pathname.split('/').filter(Boolean);
+export const onGet: RequestHandler = (event) => {
+  const segs = event.url.pathname.split('/').filter(Boolean);
   const lang = segs[0] || '';
-  throw redirect(301, `/${lang}/converters`);
+  event.redirect(301, `/${lang}/converters`);
 };
--- src/routes/converter/index.tsx
+++ src/routes/converter/index.tsx
@@ -1,6 +1,5 @@
 import type { RequestHandler } from '@builder.io/qwik-city';
-import { redirect } from '@builder.io/qwik-city';
 
-export const onGet: RequestHandler = () => {
-  throw redirect(301, `/converters`);
+export const onGet: RequestHandler = (event) => {
+  event.redirect(301, '/converters');
 };
