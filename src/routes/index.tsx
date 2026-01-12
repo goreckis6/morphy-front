1a8d2dd70955996f3a6ae10d2b24bb9e0a945204
goreckis6 <116648161+goreckis6@users.noreply.github.com>
2026-01-12 09:33:01 +0100

    jj

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,9 @@
 import { component$ } from '@builder.io/qwik';
-import type { DocumentHead } from '@builder.io/qwik-city';
+import type { DocumentHead, RequestHandler } from '@builder.io/qwik-city';
+
+export const onGet: RequestHandler = async ({ redirect }) => {
+  throw redirect(308, '/en/');
+};
 
 export default component$(() => {
   return (
